bdeea8dcc94739d96383d024823396bfe449b593
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-03-13 14:01:16 +0900

    generate tmp file, add file content test

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use assert_fs:: prelude::*;
 use std::process::Command;
 
 #[test]
@@ -10,6 +11,20 @@ fn file_dosent_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn file_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
 
     Ok(())
 }
