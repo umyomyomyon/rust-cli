024345cd09b25a53812647651414a3a88f9576ea
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-03-13 13:28:17 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-use anyhow::{Context, Result};
-
+use anyhow::Context;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -12,12 +9,29 @@ struct Cli {
 #[derive(Debug)]
 struct CustomError(String);
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{}`", &args.path.to_str().unwrap()))?;
-    println!("file conetnt: {}", content);
+        .with_context(|| format!("{}", args.path.display()))?;
+
+    find_matches(&content, &args.pattern, std::io::stdout());
+
     Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line).unwrap(); // writeln! returns Result<(), Err>
+        }
+    }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::<u8>::new(); // Vec<u8> is implemented std::io::Write.
+    find_matches("hoge\nhuga\npiyo\n", "hoge", &mut result);
+    assert_eq!(result, b"hoge");
 }
 
 
